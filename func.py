e9666f42e1340aa0f629f14d4da5cd9d1ea76cfa
salastro <salatrod@gmail.com>
2022-05-15 22:18:49 +0200

    Format structure
    Using `black`

--- func.py
+++ func.py
@@ -1,10 +1,26 @@
 from __future__ import division
 
 from pylatex import Alignat, Axis, Center, Command, Document, Plot, TikZ
+
 # Section, Subsection, Math, Figure, Matrix,
 from pylatex.utils import NoEscape
-from sympy import (Derivative, Eq, Integral, Limit, diff, factor, integrate,
-                   limit, oo, simplify, solve, sqrt, sympify, trigsimp)
+from sympy import (
+    Derivative,
+    Eq,
+    Integral,
+    Limit,
+    diff,
+    factor,
+    integrate,
+    limit,
+    oo,
+    simplify,
+    solve,
+    sqrt,
+    sympify,
+    trigsimp,
+)
+
 # from sympy.integrals.manualintegrate import manualintegrate, integral_steps
 # from sympy.integrals.risch import NonElementaryIntegral
 from sympy.abc import x
@@ -27,17 +43,17 @@ class MathDoc(Document):
         super().__init__()
         # TODO: add preamble change gemotry
 
-    def Heading(self, title, author, date=r'\today'):
-        self.preamble.append(Command('title', title))
-        self.preamble.append(Command('author', author))
-        self.preamble.append(Command('date', NoEscape(date)))
-        self.append(NoEscape(r'\maketitle'))
+    def Heading(self, title, author, date=r"\today"):
+        self.preamble.append(Command("title", title))
+        self.preamble.append(Command("author", author))
+        self.preamble.append(Command("date", NoEscape(date)))
+        self.append(NoEscape(r"\maketitle"))
 
-    def GenPdf(self, file, title, author, date=r'\today', clean_tex=True):
+    def GenPdf(self, file, title, author, date=r"\today", clean_tex=True):
         self.Heading(title, author, date)
         self.generate_pdf(file, clean_tex=clean_tex)
 
-    def GenTex(self, file, title, author, date=r'\today'):
+    def GenTex(self, file, title, author, date=r"\today"):
         self.Heading(title, author, date)
         self.generate_tex(file)
 
@@ -55,15 +71,15 @@ class MathDoc(Document):
                 print("no computable integral")
             with self.create(Alignat(numbering=True, escape=False)) as agn:
                 agn.append(latex(equation))
-                agn.append(r'=')
+                agn.append(r"=")
                 agn.append(latex(solution))
-                agn.append(r'+C') if solvable else None
+                agn.append(r"+C") if solvable else None
         except:
             error_message()
 
     def Diff(self, equation):
         try:
-            eq = equation.split(',')
+            eq = equation.split(",")
             equation = sympify(eq[0])
             n = int(eq[1]) if len(eq) == 2 else 1
             solution = equation
@@ -72,26 +88,29 @@ class MathDoc(Document):
                 equation = Derivative(equation, x)
             with self.create(Alignat(numbering=True, escape=False)) as agn:
                 agn.append(latex(equation))
-                agn.append(r'=')
+                agn.append(r"=")
                 agn.append(latex(solution))
         except:
             error_message()
 
     def Lim(self, equation):
         try:
-            eq = equation.split(',')
+            eq = equation.split(",")
             match len(eq):
-                case 1: show, a, s = equation, 0,     '+'
-                case 2: show, a, s = eq[0],    eq[1], '+'
-                case 3: show, a, s = eq[0],    eq[1], eq[2]
+                case 1:
+                    show, a, s = equation, 0, "+"
+                case 2:
+                    show, a, s = eq[0], eq[1], "+"
+                case 3:
+                    show, a, s = eq[0], eq[1], eq[2]
             solution = limit(sympify(show), x, sympify(a), s)
             if Limit(show, x, a, s) == solution:
                 solution = "No computable limit"
                 print("no computable limit")
-            print(eq, solution, show, sep='\n')
+            print(eq, solution, show, sep="\n")
             with self.create(Alignat(numbering=True, escape=False)) as agn:
                 agn.append(latex(Limit(show, x, a, s)))
-                agn.append(r'=')
+                agn.append(r"=")
                 agn.append(latex(solution))
         except:
             error_message()
@@ -102,7 +121,7 @@ class MathDoc(Document):
             solution = trigsimp(simplify(equation))
             with self.create(Alignat(numbering=True, escape=False)) as agn:
                 agn.append(latex(equation))
-                agn.append(r'=')
+                agn.append(r"=")
                 agn.append(latex(solution))
         except:
             error_message()
@@ -113,15 +132,15 @@ class MathDoc(Document):
             solution = factor(equation)
             with self.create(Alignat(numbering=True, escape=False)) as agn:
                 agn.append(latex(equation))
-                agn.append(r'=')
+                agn.append(r"=")
                 agn.append(latex(solution))
         except:
             error_message()
 
     def Sol(self, equation):
         try:
-            if '=' in equation:
-                eq = equation.split('=')
+            if "=" in equation:
+                eq = equation.split("=")
                 solution = solve(Eq(sympify(eq[0]), sympify(eq[1])))
                 x_ = True
             else:
@@ -133,58 +152,72 @@ class MathDoc(Document):
                     agn.append(latex(equation))
                 else:
                     agn.append(latex(sympify(eq[0])))
-                    agn.append(r'=')
+                    agn.append(r"=")
                     agn.append(latex(sympify(eq[1])))
-                agn.append(r'\Rightarrow')
-                agn.append(r'x=') if x_ else None
+                agn.append(r"\Rightarrow")
+                agn.append(r"x=") if x_ else None
                 agn.append(latex(solution))
         except:
             error_message()
 
     def Eval(self, equation):
         try:
-            from numpy import (arccos, arcsin, arctan, cos, exp, log, log10,
-                               pi, sin, sqrt, tan)
-            solution = eval(equation.replace('^', '**'))
+            from numpy import (
+                arccos,
+                arcsin,
+                arctan,
+                cos,
+                exp,
+                log,
+                log10,
+                pi,
+                sin,
+                sqrt,
+                tan,
+            )
+
+            solution = eval(equation.replace("^", "**"))
             equation = sympify(equation, evaluate=False)
             # solution = eval(equation)
             with self.create(Alignat(numbering=True, escape=False)) as agn:
                 agn.append(latex(equation))
-                agn.append(r'=')
+                agn.append(r"=")
                 agn.append(latex(solution))
         except:
             error_message()
 
-    def Plot(self, equation, height='6cm', width='6cm',
-             grid='both', axis_lines='middle'):
+    def Plot(
+        self, equation, height="6cm", width="6cm", grid="both", axis_lines="middle"
+    ):
         try:
             with self.create(Center()):
                 with self.create(TikZ()):
-                    plot_options = f'height={height}, width={width}, grid={grid}, axis lines={axis_lines}'
+                    plot_options = f"height={height}, width={width}, grid={grid}, axis lines={axis_lines}"
                     with self.create(Axis(options=plot_options)) as plot:
                         plot.append(Plot(name=equation, func=equation))
         except:
             error_message()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # geometry_options = {"tmargin": "1cm", "lmargin": "10cm"}
     doc = MathDoc()
-    file_name = 'full'
+    file_name = "full"
 
-    doc.Heading(title='Integral Homework', author='SalahDin Rezk')
+    doc.Heading(title="Integral Homework", author="SalahDin Rezk")
     from sympy import cos, sin
-    doc.Inte((sin(x)**2-cos(x)**2)/(cos(x)**2*sin(x)**2))
-    doc.Diff('x**x')
-    doc.Diff('x**x, 2')
-    doc.Diff('x**(1/x), 2')
-    doc.Lim('2/sin(2*x),oo')
-    doc.Sol('x+3=1')
-    doc.Sol('x+3>1')
-    doc.Eval('2^2')
-    doc.Eval('sqrt(2)')
-    doc.Eval('sin(2)')
-    doc.Eval('sin(45/cos(3))')
+
+    doc.Inte((sin(x) ** 2 - cos(x) ** 2) / (cos(x) ** 2 * sin(x) ** 2))
+    doc.Diff("x**x")
+    doc.Diff("x**x, 2")
+    doc.Diff("x**(1/x), 2")
+    doc.Lim("2/sin(2*x),oo")
+    doc.Sol("x+3=1")
+    doc.Sol("x+3>1")
+    doc.Eval("2^2")
+    doc.Eval("sqrt(2)")
+    doc.Eval("sin(2)")
+    doc.Eval("sin(45/cos(3))")
     # doc.Eval('(3i+5)-(10i+5)')
 
     doc.generate_pdf(file_name, clean_tex=True)
--- main.py
+++ main.py
@@ -1,45 +1,81 @@
 from __future__ import division
 
-from sympy import (Derivative, Eq, Function, acos, asin, atan,  # common math
-                   cos, cot, csc, dsolve, exp, ln, log, oo, pi, sec, sin, sqrt,
-                   symbols, sympify, tan)
+from sympy import (
+    Derivative,
+    Eq,
+    Function,
+    acos,
+    asin,
+    atan,  # common math
+    cos,
+    cot,
+    csc,
+    dsolve,
+    exp,
+    ln,
+    log,
+    oo,
+    pi,
+    sec,
+    sin,
+    sqrt,
+    symbols,
+    sympify,
+    tan,
+)
 from sympy.abc import x
 
 from func import MathDoc
 from gui import QtWidgets, Ui_MainWindow  # gui
 
 if __name__ == "__main__":
-    # Setup the ui.
-    import sys  # for command line arguments
-    app = QtWidgets.QApplication(sys.argv)  # Create the application.
-    MainWindow = QtWidgets.QMainWindow()  # Create the main window.
-    ui = Ui_MainWindow()  # Create the ui.
-    ui.setupUi(MainWindow)  # Setup the ui.
+    import sys
 
-    # Setup the document and buttons.
-    math_doc = MathDoc()  # The document.
-    ui.inteBt.clicked.connect(lambda: math_doc.Inte(
-        ui.expTxt.toPlainText().replace(' ', '')))
-    ui.diffBt.clicked.connect(lambda: math_doc.Diff(
-        ui.expTxt.toPlainText().replace(' ', '')))
-    ui.limBt.clicked.connect(lambda: math_doc.Lim(
-        ui.expTxt.toPlainText().replace(' ', '')))
-    ui.simpBt.clicked.connect(lambda: math_doc.Simp(
-        ui.expTxt.toPlainText().replace(' ', '')))
-    ui.factBt.clicked.connect(lambda: math_doc.Fact(
-        ui.expTxt.toPlainText().replace(' ', '')))
-    ui.solBt.clicked.connect(lambda: math_doc.Sol(
-        ui.expTxt.toPlainText().replace(' ', '')))
-    ui.plotBt.clicked.connect(lambda: math_doc.Plot(
-        (ui.expTxt.toPlainText().replace(' ', ''))))
-    ui.evalBt.clicked.connect(lambda: math_doc.Eval(
-        (ui.expTxt.toPlainText().replace(' ', ''))))
-    ui.genPdfBt.clicked.connect(lambda: math_doc.GenPdf(ui.fileTxt.toPlainText(
-        ), ui.titleTxt.toPlainText(), ui.authorTxt.toPlainText(),
-        clean_tex=True))
-    ui.genLatexBt.clicked.connect(lambda: math_doc.GenTex(ui.fileTxt.toPlainText(
-        ), ui.titleTxt.toPlainText(),
-        ui.authorTxt.toPlainText()))  # The generate tex button.
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+
+    math_doc = MathDoc()
+    ui.inteBt.clicked.connect(
+        lambda: math_doc.Inte(ui.expTxt.toPlainText().replace(" ", ""))
+    )
+    ui.diffBt.clicked.connect(
+        lambda: math_doc.Diff(ui.expTxt.toPlainText().replace(" ", ""))
+    )
+    ui.limBt.clicked.connect(
+        lambda: math_doc.Lim(ui.expTxt.toPlainText().replace(" ", ""))
+    )
+    ui.simpBt.clicked.connect(
+        lambda: math_doc.Simp(ui.expTxt.toPlainText().replace(" ", ""))
+    )
+    ui.factBt.clicked.connect(
+        lambda: math_doc.Fact(ui.expTxt.toPlainText().replace(" ", ""))
+    )
+    ui.solBt.clicked.connect(
+        lambda: math_doc.Sol(ui.expTxt.toPlainText().replace(" ", ""))
+    )
+    ui.plotBt.clicked.connect(
+        lambda: math_doc.Plot((ui.expTxt.toPlainText().replace(" ", "")))
+    )
+    ui.evalBt.clicked.connect(
+        lambda: math_doc.Eval((ui.expTxt.toPlainText().replace(" ", "")))
+    )
+    ui.genPdfBt.clicked.connect(
+        lambda: math_doc.GenPdf(
+            ui.fileTxt.toPlainText(),
+            ui.titleTxt.toPlainText(),
+            ui.authorTxt.toPlainText(),
+            clean_tex=True,
+        )
+    )
+    ui.genLatexBt.clicked.connect(
+        lambda: math_doc.GenTex(
+            ui.fileTxt.toPlainText(),
+            ui.titleTxt.toPlainText(),
+            ui.authorTxt.toPlainText(),
+        )
+    )
 
     MainWindow.show()
     sys.exit(app.exec_())
